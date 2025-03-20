22c45162eb224cd32f03485edd8525e6633a7fd5
dlaudwls <dlaudwls2022@gmail.com>
2025-03-20 18:49:27 +0900

    md4~ 작업중

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// md1
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
   spaceBetween: 30,
@@ -12,6 +13,7 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
+// md2
 var swiper = new Swiper(".swiper_md2", {
   slidesPerView: 4,
   spaceBetween: 0,
@@ -19,5 +21,40 @@ var swiper = new Swiper(".swiper_md2", {
   pagination: {
     el: ".swiper-pagination_md2",
     clickable: true,
+  },
+});
+
+// md3
+const $cursor = $('.cursor');
+const $cursorShadow = $('.cursor-shadow');
+
+$(window).mousemove(function(e) {    
+    $cursor.css({
+        top: e.clientY,
+        left: e.clientX
+    });
+    
+    $cursorShadow.css({
+        top: e.clientY,
+        left: e.clientX
+    });
+});
+
+$('.cursor-big').mouseenter(function() {
+    $('html').addClass('need-to-cursor-big');
+});
+
+$('.cursor-big').mouseleave(function() {
+    $('html').removeClass('need-to-cursor-big');
+});
+
+//md4
+var swiper = new Swiper(".swiper_md4", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
