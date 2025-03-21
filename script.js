90b43da5fcc0e02deeaa2c0a56effe6e4df89702
dlaudwls <dlaudwls2022@gmail.com>
2025-03-21 17:11:20 +0900

    얼추 끝 수정필요

--- script.js
+++ script.js
@@ -58,3 +58,18 @@ var swiper = new Swiper(".swiper_md4", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+//sns
+var swiper = new Swiper(".swiper_sns", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
